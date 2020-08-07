7ece5cb5851e50dadf536ef21b8346b24ecae862
Scott Updike <supdike314159@gmail.com>
2020-08-07 15:46:00 -0400

    More cleanup.  Sheesh

--- src/main/java/io/updike/awstesting/Config.java
+++ src/main/java/io/updike/awstesting/Config.java
@@ -11,13 +11,17 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 public class Config {
 
 	private static final Regions region = Regions.US_EAST_1;
+
+	//This is where you would centrally control all of the AWS service information, like endpoints, etc.  
+	//Here I hard-coded the endpoint, but ideally it would be a property set by the appropriate environment-based
+	//property file.
 	
 	@Bean
-    public S3Wrapper s3Client() {
+	public S3Wrapper s3Client() {
 		AmazonS3ClientBuilder s3 = AmazonS3ClientBuilder.standard();
-		s3.setEndpointConfiguration(new EndpointConfiguration("http://127.0.0.1:4566",region.toString()));
+		s3.setEndpointConfiguration(new EndpointConfiguration("http://127.0.0.1:4566", region.toString()));
 		S3Wrapper wrapper = new S3Wrapper();
 		wrapper.setAmazonS3(s3.build());
 		return wrapper;
-    }
+	}
 }
